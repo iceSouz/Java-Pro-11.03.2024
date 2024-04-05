0f1d32558fbef043ee63a384c79c488086f3e343
Stas <macbook@MacBook-Air-Stas.local>
2024-04-05 18:27:20 +0300

    Implement functionality for dictionaries and sets

---
+++ src/coffee/order/Order.java
@@ -0,0 +1,27 @@
+package coffee.order;
+
+public class Order {
+    private final int number;
+    private final String name;
+
+    public Order(int number, String name) {
+        this.number = number;
+        this.name = name;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Order{" +
+                "number=" + number +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
